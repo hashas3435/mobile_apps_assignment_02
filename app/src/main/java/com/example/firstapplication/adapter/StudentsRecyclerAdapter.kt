bbb2b07c7d235a8988b18ad03a8bea1ef1cb2b2f
omer hasid <omerhasid11@gmail.com>
2025-01-14 11:57:18 +0200

    add student adapter

--- app/src/main/java/com/example/firstapplication/adapter/StudentsRecyclerAdapter.kt
+++ app/src/main/java/com/example/firstapplication/adapter/StudentsRecyclerAdapter.kt
@@ -1,4 +1,35 @@
 package com.example.firstapplication.adapter
 
-class StudentsRecyclerAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firstapplication.R
+import com.example.firstapplication.model.Student
+
+
+class StudentsRecyclerAdapter(private var students: List<Student>?): RecyclerView.Adapter<StudentViewHolder>() {
+
+    var listener: OnItemClickListener? = null
+
+    fun update(students: List<Student>?) {
+        this.students = students
+    }
+
+    override fun getItemCount(): Int = students?.size ?: 0
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StudentViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(
+            R.layout.student_list_row,
+            parent,
+            false
+        )
+        return StudentViewHolder(itemView, listener)
+    }
+
+    override fun onBindViewHolder(holder: StudentViewHolder, position: Int) {
+        holder.bind(
+            student = students?.get(position),
+            position = position
+        )
+    }
 }
