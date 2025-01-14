9c0d2e791960e534f1166e6b4a5db54112ca592e
omer hasid <omerhasid11@gmail.com>
2025-01-14 11:57:41 +0200

    add students list view activity

--- app/src/main/java/com/example/firstapplication/ViewStudentsActivity.kt
+++ app/src/main/java/com/example/firstapplication/ViewStudentsActivity.kt
@@ -1,12 +1,21 @@
 package com.example.firstapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firstapplication.adapter.OnItemClickListener
+import com.example.firstapplication.adapter.StudentsRecyclerAdapter
+import com.example.firstapplication.model.Model
+import com.example.firstapplication.model.Student
 
 class ViewStudentsActivity : AppCompatActivity() {
+    private var students: MutableList<Student>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +25,26 @@ class ViewStudentsActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        students = Model.shared.students
+        val recyclerView: RecyclerView = findViewById(R.id.view_students_activity)
+        recyclerView.setHasFixedSize(true)
+
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+
+        val adapter = StudentsRecyclerAdapter(students)
+
+        adapter.listener = object : OnItemClickListener {
+            override fun onItemClick(position: Int) {
+                Log.d("TAG", "On click Activity listener on position $position")
+            }
+
+            override fun onItemClick(student: Student?) {
+                Log.d("TAG", "On student clicked name: ${student?.name}")
+            }
+        }
+
+        recyclerView.adapter = adapter
     }
 }
